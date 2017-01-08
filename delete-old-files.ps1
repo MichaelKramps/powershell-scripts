0775a87bbb0a7b5c086044d5d78642b8ca770561
Michael Kramps <mdk989@gmail.com>
2017-01-07 18:03:57 -0600

    added -recurse to traverse all children and added a filetype check

--- delete-old-files.ps1
+++ delete-old-files.ps1
@@ -1,14 +1,13 @@
-# This short script takes an array of directory files paths 
-# and a defined number of years
-# It finds all files that were last accessed more than $xYears ago
+# This short script takes an array of directory files paths,
+# a defined number of years, and an array of file types
+# It recursively finds all files of the specified types that were last accessed more than $xYears ago
 # Then deletes them upon confirmation
 
-$directories = @("C:\Users\Michael\Downloads", "C:\Users\Michael\Documents")
-$xYears = 2
+$directories = @("C:\Users\Michael\test", "C:\Users\Michael\test2")
+$xYears = 0
+$fileTypes = @("*.jpg", "*.png", "*.gif")
 
 $currentDate = get-date
 $xYearsAgo = $currentDate.addyears(-$xyears)
 
-$directories | foreach-object {get-childitem -path $_ | where-object -property lastaccesstime -lt $xYearsAgo | remove-item -confirm}
-
-# To speed up process, you could remove the -confirm parameter
+$directories | foreach-object {get-childitem -path $_ -recurse -include $fileTypes | where-object -property lastaccesstime -lt $xYearsAgo | remove-item -confirm}
